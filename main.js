6846f72580522a73e00f944f90d1f5a841a36fef
HaykelHadjSalem <haykel_hadj@yahoo.fr>
2020-08-17 15:52:31 +0100

    adding text input and submit button

--- main.js
+++ main.js
@@ -10,3 +10,4 @@ function makeBook(title, author, genre, pageNumbers, year, language) {
 	};
 }
 var book1 = makeBook("Harry Potter and the Sorcerer's Stone", 'J. K. Rowling', 'Fantasy',256 ,1997, ['English', 'French']);
+var book = makeBook("To Kill a Mockingbird", )
